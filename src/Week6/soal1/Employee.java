842e943b8848b9b4a7b04fa154ce1221f1b4e46e
Ahmad Bukhori <bukhori.fahri27@gmail.com>
2026-04-01 08:19:54 +0700

    Adding Week6 (Soal 1 - 3)

---
+++ src/Week6/soal1/Employee.java
@@ -0,0 +1,24 @@
+package Week6.soal1;
+
+public class Employee {
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{name='" + name + "', salary=" + salary + "}";
+    }
+}
